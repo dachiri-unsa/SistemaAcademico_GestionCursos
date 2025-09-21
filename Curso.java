117058a91143a6e2a4b5c7ead8b3f4ef87c2ddf4
Leo_Bv <lcavero@unsa.edu.pe>
2025-09-20 22:39:27 -0500

    Cursos

--- Curso.java
+++ Curso.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+
 public class Curso {
-    
+    private int código;
+    private String nombreCurso;
+    private Docente docente;
+    public Curso(Scanner sc, int código){
+        System.out.println("Ingresar nombre del curso: ");
+        nombreCurso = sc.nextLine();
+        System.out.println("Ingresar código del curso: ");
+        código = sc.nextInt();
+        this.código = código;
+    }
+
+    public String getNombreCurso(){
+        return nombreCurso;
+    }
+    public void setNombreCurso(String nombreCurso){
+        this.nombreCurso = nombreCurso;
+    }
+    public Docente getDocente(){
+        return docente;
+    }
+    public void setApellidos(Docente docente){
+        this.docente = docente;
+    }
+    public int getCódigo(){
+        return código;
+    }
+    public void setCodigo(int código){
+        this.código = código;
+    }
+    public String toString(){
+        return "Curso\nNombre:"+nombreCurso+", código: "+código+", docente: "+docente;
+    }
 }
